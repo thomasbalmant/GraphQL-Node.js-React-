90d2f403288beee93cfd952a85b89d5b0c19e01d
Thomas <thomas_dru@hotmail.com>
2022-07-23 14:57:00 -0300

    front ok

---
+++ frontend/src/components/NewUserForm.tsx
@@ -0,0 +1,45 @@
+import { useState, FormEvent } from 'react'
+import { gql, useMutation } from '@apollo/client'
+import { GET_USER } from '../App'
+
+const CREATE_USER = gql`
+  mutation($name: String!) {
+    createUser(name: $name) {
+      id
+      name
+    }
+  }
+`
+
+export function NewUserForm() {
+  const [name, setName] = useState('')
+  const [createUser, { data, loading, error }] = useMutation(CREATE_USER)
+
+  async function handleCreateUser(event: FormEvent) {
+    event.preventDefault()
+
+    if (!name) {
+      return
+    }
+
+    await createUser({
+      variables: {
+        name,
+      },
+      refetchQueries: [GET_USER], //CHAMA O METODO GET_USER PARA ATUALIZAR A LISTA DOS USUARIOS CONTENDO O NOVO USUARIO CADASTRADO
+      onCompleted: () => setName(''),
+      onError: () => {}
+    })
+  }
+
+  return (
+    <form onSubmit={handleCreateUser}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button type="submit">Enviar</button>
+    </form>
+  )
+}
